c09b48c406a4a19e0bc85192e95e6e150008820b
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-10 16:36:32 +0900

    feat(Order) : create skeleton code
    - Order object store order information(date, menu, orderAmount,totalPrice)
    - check possibility of order with order information

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -1,5 +1,18 @@
 package christmas;
 
 public class Order {
+    Date date;
+    Menu.menuGroup orderMenu;
+    int orderAmount;
+    int totalPrice;
+    Order(Date date, Menu.menuGroup order, int amount){
+        this.date = date;
+        this.orderMenu = order;
+        this.orderAmount = amount;
+        this.totalPrice = orderMenu.getPrice() * amount;
+    }
 
+    public void checkOrderPossibility(){
+
+    }
 }
