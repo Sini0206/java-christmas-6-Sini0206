48e45abbc579599f02b6523832207f1e9f06a0f9
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-11 16:14:53 +0900

    feat(Event): check if the customer is a target for events
    add 2 variables and 1 method
    - checkEventPossible(): if total price of the order is over 10000, the customer is target for events

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -1,4 +1,13 @@
 package christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
+    boolean isEventPossible = false;
+    Order order;
+    public void checkEventPossible(Order order){
+        if(order.getTotalPrice() >= 10000)
+            isEventPossible = true;
+    }
 }
