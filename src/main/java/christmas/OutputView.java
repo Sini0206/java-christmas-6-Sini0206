f5b3ea98ba1e497cd3eb6ef234b1a4a5fd68fb34
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-11 21:52:42 +0900

    feat(OutputView): print ordered menu name and the number of menu dish

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,2 +1,10 @@
-package christmas;public class OutputView {
+package christmas;
+
+public class OutputView {
+    static public void print(Order order){
+        System.out.println("<주문 메뉴>");
+        for (int i = 0; i < order.getOrderMenu().size(); i++) {
+            System.out.println(order.getOrderMenu().get(i).name()+" "+ order.getDishAmount().get(i) +"개");
+        }
+    }
 }
