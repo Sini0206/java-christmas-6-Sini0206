21510c73181f1447372580e4b5329a7317753473
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-11 20:29:31 +0900

    feat(Date): classify type of days
    add 2 variables, 1 enum, 1 method
    - type of day can be SATURDAY or SUNDAY or HOLIDAY or not

--- src/main/java/christmas/Date.java
+++ src/main/java/christmas/Date.java
@@ -2,7 +2,25 @@ package christmas;
 
 public class Date {
     int day;
+    dayType type;
     Date(int inputDay){
         day = inputDay;
+        //type = classifyType();
     }
+    enum dayType{
+        SATURDAY,SUNDAY,HOLIDAY
+    }
+    private dayType classifyType(){
+        if (day == 25)
+            type = dayType.HOLIDAY;
+        for (int week = 0; week < 5 ; week++) {
+            if(day == 3 + 7 * week)
+                type = dayType.SUNDAY;
+            if(day == 2 + 7 * week)
+                type = dayType.SATURDAY;
+        }
+
+        return type;
+    }
+
 }
