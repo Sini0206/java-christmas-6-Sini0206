df547b7d898ff0ba841e8bb52d0861fad07fa194
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-11 23:22:40 +0900

    feat(Event): check if the date is during the particular event
    add 2 variables, 1 enum, 1 method
    - checkEventPeriod(): if the date is during 1 ~ 31, December Event is possible. if the date is during 1 ~ 25, Christmas D-Day Discount Event is also possible.

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Event {
     static boolean isEventPossible = false;
     static boolean presentChampagne = false;
+    boolean isDecemberEvent = false;
+    boolean isDDayEvent = false;
     Date date;
     static public void checkEventPossible(Order order){
         if(order.getTotalPrice() >= 10000)
@@ -15,5 +17,18 @@ public class Event {
     public void presentEvent(Order order){
         if(order.getTotalPrice() > 120000)
             presentChampagne = true;
+    }
+    
+    public void checkEventPeriod(){
+        if(isEventPossible){
+            if(date.day >= 1 && date.day <= 31)
+                isDecemberEvent = true;
+            if(date.day >= 1 && date.day <= 25)
+                isDDayEvent = true;
+        }
+    }
+
+    enum decemberEvent{
+        WEEKDAY_EVENT, WEEKEND_EVENT, SPECIAL_EVENT
     }
 }
