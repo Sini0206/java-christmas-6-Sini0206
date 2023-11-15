0411330862be021bf6bcd589fc7e833482d24498
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-11-15 20:53:51 +0900

    feat(InputView): check validate date
    add 1 method
    - validateDate(): throw error IllegalArgumentException when the input format is not number or when the date is not in range(1 ~ 31)
    - if error ocurred, operate readDate() again

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Application {
 
     static Date date;
+    static Order order;
+    static Event event;
     static String [] menuNameNCount = new String[2];
     static List<Menu.dishInfo> orderMenu = new ArrayList<>(); //  나중에 Order orderMenu로 전달
     static List<Integer> dishCount = new ArrayList<>();    //  나중에 Order(amount)로 전달
-    static Order order;
-    static Event event;
 
     public static void storeMenuNames(String[] input){
         List<String> menuNames = new ArrayList<>();     // 변환용(String -> Menu.dishInfo)
@@ -21,21 +21,31 @@ public class Application {
         }
 
         for (int i = 0; i < menuNames.size(); i++) {
-            orderMenu.add(Menu.dishInfo.valueOf(menuNames.get(i)));
+            orderMenu.add(Menu.dishInfo.valueOf(menuNames.get(i))); //  "해산물파스타" -> 해산물파스타
         }
     }
     public static void storeAmount(String [] input){
         for (int i = 0; i < input.length; i++) {
-            menuNameNCount = input[i].trim().split("-"); //  "해산물파스타","2"
+            if(Integer.parseInt(menuNameNCount[1]) < 1){
+                throw new IllegalArgumentException();
+            }
             dishCount.add(Integer.parseInt(menuNameNCount[1]));    // "2" -> 2
         }
     }
 
     public static void orderGenerator(){
-        String [] input = InputView.readOrder();
-        storeMenuNames(input);
-        storeAmount(input);
-        order = new Order(date,orderMenu,dishCount);
+        boolean validateOrder = true;
+        try{
+            String [] input = InputView.readOrder();
+            storeMenuNames(input);
+            storeAmount(input);
+            order = new Order(date,orderMenu,dishCount);
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.");
+            validateOrder = false;
+        }
+        if(!validateOrder)
+            orderGenerator();
     }
 
     public static void dateGenerator(){
@@ -51,8 +61,13 @@ public class Application {
         OutputView.printPayment();
         OutputView.printBadge();
     }
+    public static void validateMenu(){
+
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
         dateGenerator();
 
         orderGenerator();
--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -2,20 +2,46 @@ package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
+    public static int readDate() throws IllegalArgumentException{
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int inputDate = 0;
 
-    public static int readDate(){
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
-                "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        return Integer.parseInt(Console.readLine());
+        try {
+            String input = Console.readLine();
+
+            if (validateDate(input)){
+                inputDate = Integer.parseInt(input);
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            inputDate = readDate();
+        }
+        return inputDate;
     }
     public static String[] readOrder(){
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String [] input = Console.readLine().trim().split(","); // input:["해산물파스타-2","레드와인-1","초코케이크-1"]
+        String [] inputOrder = Console.readLine().trim().split(","); // input:["해산물파스타-2","레드와인-1","초코케이크-1"]
 
-        return input;
+        return inputOrder;
+    }
+
+    private static boolean validateDate(String input) {
+        try{    //  방문 날짜가 1에서 31 사이의 숫자가 아닌 경우
+            if(Integer.parseInt(input) > 31 || Integer.parseInt(input) < 1) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        return true;
+    }
+
+    private static void handleException(){
+
     }
 }
